44a51496d60256c1a97f373cabb13364ad644a05
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-06-16 17:10:57 +0200

    Added docker-build.sh script.

---
+++ docker-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -exu
+if [ -z $BUILD_NUMBER ]; then
+    echo This should run under jenkins only
+    exit 1
+fi
+
+BASE_NAME="cloudesire/catwatcher"
+BUILD_VERSION=$BASE_NAME:$BUILD_NUMBER
+BUILD_LATEST=$BASE_NAME:latest
+
+docker build --no-cache --force-rm -t $BUILD_VERSION .
+docker push $BUILD_VERSION
+docker tag -f $BUILD_VERSION $BUILD_LATEST
+docker push $BUILD_LATEST
+docker rmi $BUILD_VERSION $BUILD_LATEST
+exit 0
