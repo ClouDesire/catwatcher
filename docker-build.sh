f7d3c2d7257d888ebe8ab95a41164898322b9278
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-06-18 18:15:56 +0200

    Added login in docker-build script

--- docker-build.sh
+++ docker-build.sh
@@ -9,6 +9,8 @@ BUILD_VERSION=$BASE_NAME:$BUILD_NUMBER
 BUILD_LATEST=$BASE_NAME:latest
 
 docker build --no-cache --force-rm -t $BUILD_VERSION .
+
+docker login -e "jenkins@cloudesire.com" -u $PUBLIC_REGISTRY_USERNAME -p $PUBLIC_REGISTRY_PASSWORD
 docker push $BUILD_VERSION
 docker tag -f $BUILD_VERSION $BUILD_LATEST
 docker push $BUILD_LATEST
