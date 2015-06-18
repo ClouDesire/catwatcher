b4bd384b4b7ac447fe7e37a5097c54f60092c71e
Giovanni Toraldo <g.toraldo@liberologico.com>
2015-06-18 12:46:02 +0200

    Dockerfile: do not recreate catwatcher config on every run

--- docker/run.sh
+++ docker/run.sh
@@ -2,13 +2,12 @@
 
 if [ ! -f /.tomcat_admin_created ]; then
     source /create_tomcat_admin_user.sh
-fi
-
-mkdir -p /etc/catwatcher
-
-echo "username=admin
+    mkdir -p /etc/catwatcher
+    echo "username=admin
 password=$PASS
 endpoint=http://localhost:8080
 sleepTime=30" > /etc/catwatcher/catwatcher.properties
+fi
 
 exec ${CATALINA_HOME}/bin/catalina.sh run
+
